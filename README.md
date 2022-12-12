284c2c3666b8f2e68d3348c3b31acee1168e65f2
philipdoucla <philipminhdo@ucla.edu>
2022-12-12 12:26:47 -0800

    Initial commit of new format, basic page layouts and favicon

--- README.md
+++
@@ -1,4 +0,0 @@
-# ewd-2022
-Website for 2022 Engineering Welcome Day 
-
-ESUC 2022
--- linkify.py
+++
@@ -1,4 +0,0 @@
-import clipboard
-while True: 
-    text = input('Paste a link:')
-    clipboard.copy("<a href=\"{}\">{}</a>".format(text, text))
